ee4907edfe089dd20ae82873246ac0e0ce78713c
Mohamed Aitichou <mohamedaitichou1@gmail.com>
2025-10-19 16:15:39 +0200

    add link deployed application5

--- ticketing/src/main/java/com/example/ticketing/security/SecurityConfig.java
+++ ticketing/src/main/java/com/example/ticketing/security/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
         cors.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://ticketing-jo-2024-1.onrender.com"
+                "https://ticketing-jo-2024-2.onrender.com"
         ));
 
         cors.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
